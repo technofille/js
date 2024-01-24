122c636125f6aeb1ca44bc60a3db0849eb9f4607
technofille <157573061+technofille@users.noreply.github.com>
2024-01-24 10:57:57 +0000

    changes

---
+++ datatypes-summary.js
@@ -0,0 +1,25 @@
+const score = 100;
+const scoreValue = 30;
+const isLoggedIn = false;
+const temp = null;
+let userEmail;
+const id = Symbol('123');
+const anotherId = Symbol('123');
+// console.log(id);
+// console.log(typeof id);
+// console.log(anotherId);
+// console.log(typeof anotherId);
+
+const bigNum = 8765432345678987654345678n
+
+const heros = ["test","test2","test3"]
+let myObj ={
+    name:"test4",
+    age: 22,
+}
+
+const myFunction = function(){
+     console.log("test")
+}
+
+console.log(typeof myFunction);
