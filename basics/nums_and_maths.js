0e309849e38a29f596ff868c7651f8e078eb08b0
technofille <157573061+technofille@users.noreply.github.com>
2024-01-24 12:35:09 +0000

    number n maths

---
+++ basics/nums_and_maths.js
@@ -0,0 +1,19 @@
+const score = 200
+//console.log(score);
+const balance = new Number(200.243432424424);
+//console.log(balance);
+//console.log(balance.toFixed(4));
+//console.log(balance.toString());
+
+const otherNUmber = 23.838132;
+//console.log(otherNUmber.toPrecision(2));
+
+const newNumber = 1000000
+//console.log(newNumber.toLocaleString('en-IN'))
+
+/////MATHS
+
+// console.log(Math);
+// console.log(Math.abs(-90));
+//console.log(Math.round(90.7483473));
+//console.log(Math.floor(90.7483473));
---
+++ basics/string.js
@@ -0,0 +1,24 @@
+const Name = "krupali "
+const repoCount = 50
+//console.log(Name + repoCount + " check");
+//console.log(`test efsff fsdfsdf fews ${Name} njkbnjknjknjkm kjmknkj mkm lm ${repoCount}`);
+
+const gameName = new String('hitesh-hc-com');
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.indexOf('s'));
+//console.log(gameName.indexOf('s'));
+//const newGameName = gameName.substring(3,7)
+//console.log(newGameName);
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+const newString = "          tst           ";
+//console.log(newString.trim());
+const url ="https://test.com/test%20teas";
+//console.log(url.replace('%20','-'));
+//console.log(url.includes('t'));
+
+console.log((gameName.split('-')));
