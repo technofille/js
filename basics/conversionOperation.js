13a25cf620f616c6812c699444ec1318677b9bd9
technofille <157573061+technofille@users.noreply.github.com>
2024-01-24 09:47:32 +0000

    changes

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -20,4 +20,28 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let sNumber = 33
 let stringNumber = String(sNumber);
-console.log(typeof (stringNumber));
+//console.log(typeof (stringNumber));
+
+// operations
+let value = 3
+let negValue = -value
+//console.log(negValue)
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/4);
+//console.log(2%3);
+
+
+let str1 = "test"
+let str2 = " now !"
+let str3 = str1 + str2
+console.log(str3);
+
+
+//toPrimitive in ecma
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
